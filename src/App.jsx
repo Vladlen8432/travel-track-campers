abd3e4edb417a467c50a89a0fa3744134a07ada9
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-12-03 18:27:23 +0200

    Checkbox completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import CatalogPage from "./pages/CatalogPage";
-import CamperDetails from "./components/CamperDetails";
-import Features from "./components/Features";
-import Reviews from "./components/Reviews";
 
 const App = () => {
   return (
@@ -13,11 +10,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/campers" element={<CatalogPage />} />
-        <Route path="/campers/:id" element={<CamperDetails />}>
-          <Route index element={<Features />} />
-          <Route path="features" element={<Features />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
       </Routes>
     </div>
   );
--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,118 +1,141 @@
-import { useState } from "react";
 import css from "./styles/Catalog.module.css";
-import { useEffect } from "react";
-import { fetchCampers } from "../services/api";
-import { Link } from "react-router-dom";
 
 const Catalog = () => {
-  const [campers, setCampers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetchCampers().then((data) => {
-      setCampers(data);
-      setLoading(false);
-    });
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-
   return (
     <div className={css.containerCatalog}>
-      <div className={css.containerFilter}>
-        <form>
-          <div>
-            <label htmlFor="location">Location</label>
-            <input type="text" />
+      <div className={css.containerFilters}>
+        <form className={css.filtersForm}>
+          <div className={css.location}>
+            <label className={css.labelLocation} htmlFor="location">
+              Location
+            </label>
+            <br />
+            <input
+              className={css.inputLocation}
+              type="text"
+              value="Kyiv, Ukraine"
+            />
           </div>
 
-          <div>
-            <label htmlFor="filters">Filters</label>
-            <h3>Vehicle equipment</h3>
-            <input type="checkbox" />
-            <input type="checkbox" />
-            <input type="checkbox" />
-            <h3>Vehicle type</h3>
-            <input type="checkbox" />
-            <input type="checkbox" />
-            <input type="checkbox" />
+          <div className={css.filters}>
+            <label className={css.labelFilters} htmlFor="filters">
+              Filters
+            </label>
+            <h3 className={css.titleType}>Vehicle equipment</h3>
+            <hr className={css.line} />
+
+            <div className={css.checkboxGroup}>
+              <label className={css.checkbox}>
+                <input type="checkbox" value="AC" />
+                <div className={css.iconWrapper}>
+                  <img src="/path-to-ac-icon.svg" alt="AC Icon" />
+                  <span>AC</span>
+                </div>
+              </label>
+
+              <label className={css.checkbox}>
+                <input type="checkbox" value="Automatic" />
+                <div className={css.iconWrapper}>
+                  <img src="/path-to-automatic-icon.svg" alt="Automatic Icon" />
+                  <span>Automatic</span>
+                </div>
+              </label>
+
+              <label className={css.checkbox}>
+                <input type="checkbox" value="Kitchen" />
+                <div className={css.iconWrapper}>
+                  <img src="/path-to-kitchen-icon.svg" alt="Kitchen Icon" />
+                  <span>Kitchen</span>
+                </div>
+              </label>
+            </div>
+
+            <h3 className={css.titleType}>Vehicle type</h3>
+            <hr className={css.line} />
+
+            <div className={css.checkboxGroup}>
+              <label className={css.checkbox}>
+                <input type="checkbox" value="Van" />
+                <div className={css.iconWrapper}>
+                  <img src="/path-to-kitchen-icon.svg" alt="Kitchen Icon" />
+                  <span>Van</span>
+                </div>
+              </label>
+
+              <label className={css.checkbox}>
+                <input type="checkbox" value="Truck" />
+                <div className={css.iconWrapper}>
+                  <img src="/path-to-kitchen-icon.svg" alt="Kitchen Icon" />
+                  <span>Truck</span>
+                </div>
+              </label>
+
+              <label className={css.checkbox}>
+                <input type="checkbox" value="SUV" />
+                <div className={css.iconWrapper}>
+                  <img src="/path-to-kitchen-icon.svg" alt="Kitchen Icon" />
+                  <span>SUV</span>
+                </div>
+              </label>
+            </div>
           </div>
+
           <button type="submit">Search</button>
         </form>
       </div>
 
-      <div className={css.containerContent}>
-        {campers.length > 0 ? (
-          <ul className={css.listCamper}>
-            {campers.map((camper) => (
-              <li className={css.listItemCamper} key={camper.id}>
-                <img
-                  className={css.imageCamper}
-                  src={camper.gallery[0].thumb}
-                  alt="camper"
-                />
-                <div className={css.camperOverlay}>
-                  <div className={css.namePrice}>
-                    <h3>{camper.name}</h3>
-                    <div className={css.priceFavorite}>
-                      <h3 className={css.price}>{camper.price}</h3>
-                      <img src="" alt="heart" />
-                    </div>
-                  </div>
-
-                  <div className={css.containerReviewsLocation}>
-                    <p className={css.reviews}>
-                      <span>
-                        <img src="" alt="star" />
-                      </span>
-                      {camper.reviews.length > 0
-                        ? `${(
-                            camper.reviews.reduce(
-                              (sum, review) => sum + review.reviewer_rating,
-                              0
-                            ) / camper.reviews.length
-                          ).toFixed(1)} (${camper.reviews.length} Reviews)`
-                        : "No Reviews"}
-                    </p>
-                    <p className={css.location}>{camper.location}</p>
-                  </div>
-
-                  <p className={css.camperDescription}>{camper.description}</p>
-
-                  <div className={css.featureContainer}>
-                    {[
-                      { label: camper.transmission },
-                      { label: camper.engine },
-                      { label: "Air Conditioning", condition: camper.AC },
-                      { label: "Bathroom", condition: camper.bathroom },
-                      { label: "Kitchen", condition: camper.kitchen },
-                      { label: "TV", condition: camper.TV },
-                      { label: "Radio", condition: camper.radio },
-                      { label: "Refrigerator", condition: camper.refrigerator },
-                      { label: "Microwave", condition: camper.microwave },
-                      { label: "Gas", condition: camper.gas },
-                      { label: "Water", condition: camper.water },
-                    ]
-                      .filter(
-                        (item) => item.condition === undefined || item.condition
-                      )
-                      .map((item, index) => (
-                        <div key={index} className={css.featureItem}>
-                          <p className={css.featureText}>{item.label}</p>
-                        </div>
-                      ))}
-                  </div>
-                  <Link to={camper.id.toString()}>Show more</Link>
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No campers found.</p>
-        )}
-      </div>
+      <div className={css.mainContentCatalog}>Campers list</div>
     </div>
   );
 };
 
 export default Catalog;
+
+// import css from "./styles/Catalog.module.css";
+
+// const Catalog = () => {
+//   return (
+//     <div className={css.containerCatalog}>
+//       <div className={css.containerFilters}>
+//         <form className={css.filtersForm}>
+//           <div className={css.location}>
+//             <label className={css.labelLocation} htmlFor="location">
+//               Location
+//             </label>
+//             <br />
+//             <input
+//               className={css.inputLocation}
+//               type="text"
+//               value="Kyiv, Ukraine"
+//             />
+//           </div>
+
+//           <div className={css.filters}>
+//             <label className={css.labelFilters} htmlFor="filters">Filters</label>
+//             <h3 className={css.titleType}>Vehicle equipment</h3>
+//             <hr className={css.line} />
+
+//             <div className={css.checkboxGroup}>
+
+//             </div>
+
+//             <input type="checkbox" value='AC' />
+//             <input type="checkbox" />
+//             <input type="checkbox" />
+
+//             <h3 className={css.titleType}>Vehicle type</h3>
+
+//             <input type="checkbox" />
+//             <input type="checkbox" />
+//             <input type="checkbox" />
+//           </div>
+//           <button type="submit">Search</button>
+//         </form>
+//       </div>
+
+//       <div className={css.mainContentCatalog}>Campers list</div>
+//     </div>
+//   );
+// };
+
+// export default Catalog;
